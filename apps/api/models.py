c048321d9b1ff99fe5527f0d70934af9144c4643
gsalas31 <grace_salas94@hotmail.com>
2020-09-16 14:15:52 -0400

    Serializers

--- apps/api/models.py
+++ apps/api/models.py
@@ -9,6 +9,8 @@ class Category(models.Model):
 
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,18 +1,32 @@
-# from rest_framework import serializers
-# from apps.api.models import Tutorial, Product
-#
-# class TutorialSerializer(serializers.ModelSerializer):
-#     #can i do  'owner.fullname' ?
-#     owner = serializers.ReadOnlyField(source='owner.fullname')
-#
-#     class Meta:
-#         model = Tutorial
-#         fields = ('id', 'title', 'description', 'owner','image', 'material', 'procedure', 'created_at', 'updated_at')
-#
-#
-# class ProductSerializer(serializers.ModelSerializer):
-#     owner = serializers.ReadOnlyField(source='owner.fullname')
-#
-#     class Meta:
-#         model = Product
-#         fields = ('id', 'description', 'owner','image', 'created_at', 'updated_at')
+from rest_framework import serializers
+from apps.api.models import Category, Product, Comment, Tutorial
+
+class CategorySerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'owner', 'created_at', 'updated_at')
+
+class ProductSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Product
+        fields = ('id', 'image', 'description', 'owner', 'created_at', 'updated_at')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'subject', 'description', 'owner', 'created_at', 'updated_at')
+
+
+class TutorialSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Tutorial
+        fields = ('id', 'title', 'description', 'image', 'materials', 'procedure', 'owner', 'created_at', 'updated_at')
